6cfeae04352084073c86fcd90cff97e7acf8f161
rileykc98 <rileykilmerchoi@hotmail.com>
2020-04-01 20:05:08 -0400

    code cleanup

--- test/test.js
+++ test/test.js
@@ -1,17 +1,19 @@
 const app = require("../app");
-
 const request = require("supertest");
 const expect = require("chai").expect;
 
 const MONGODB_URI =
   "mongodb+srv://soen341:soen341@clustersoen341-bbtjh.mongodb.net/UserData?retryWrites=true&w=majority";
+const username = "test";
+const userID = "5e84149dde6be044e854a069";
+const password = "test";
 
 var imgname;
 
 describe("LOGIN TEST", function() {
   it("correct credentials", done => {
     request(app)
-      .post("/login_page/test/test")
+      .post("/login_page/" + username + "/" + password)
       .then(res => {
         const body = res.body;
         //console.log(body);
@@ -22,7 +24,7 @@ describe("LOGIN TEST", function() {
 
   it("incorrect credentials", done => {
     request(app)
-      .post("/login_page/test/wrong")
+      .post("/login_page/" + username + "/wrong")
       .then(res => {
         const body = res.body;
         //console.log(body);
@@ -36,7 +38,7 @@ describe("POST PHOTO TEST", function() {
   it("upload photo", done => {
     imgname = date() + "demo-image-01.jpg";
     request(app)
-      .post("/upload/5e84149dde6be044e854a069")
+      .post("/upload/" + userID)
       .attach("file", "front_end/img/demo-image-01.jpg")
       .end(function(err, res) {
         done();
@@ -47,7 +49,7 @@ describe("POST PHOTO TEST", function() {
 describe("LIKE TEST", function() {
   it("like photo", done => {
     request(app)
-      .post("/like_photo/test/" + imgname + "/test")
+      .post("/like_photo/" + username + "/" + imgname + "/" + username)
       .then(res => {
         const body = res.body;
         //console.log(body);
@@ -61,7 +63,13 @@ describe("COMMENT TEST", function() {
   it("leave a comment", done => {
     request(app)
       .post(
-        "/leaveComment/5e84149dde6be044e854a069/test/5e84149dde6be044e854a069/" +
+        "/leaveComment/" +
+          userID +
+          "/" +
+          username +
+          "/" +
+          userID +
+          "/" +
           imgname
       )
       .send({ comment: "testing" })
@@ -90,7 +98,7 @@ describe("DELETE PHOTO TEST", function() {
 describe("FOLLOW TEST", function() {
   it("follow a user", done => {
     request(app)
-      .post("/follow_page/5e84149dde6be044e854a069/5e7e99e636863928902668e5")
+      .post("/follow_page/" + userID + "/5e7e99e636863928902668e5")
       .then(res => {
         const body = res.body;
         // console.log(body);
@@ -101,7 +109,7 @@ describe("FOLLOW TEST", function() {
 
   it("unfollow a user", done => {
     request(app)
-      .post("/follow_page/5e84149dde6be044e854a069/5e7e99e636863928902668e5")
+      .post("/follow_page/" + userID + "/5e7e99e636863928902668e5")
       .then(res => {
         const body = res.body;
         //console.log(body);
