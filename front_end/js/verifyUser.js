377a51d655a82efe1456a71c5f640089333b2c9f
Nicholas <ngidaro@hotmail.com>
2020-04-08 15:53:51 -0400

    Cleaned the code, and fixed BUG-132-Show comments displays error

--- front_end/js/verifyUser.js
+++ front_end/js/verifyUser.js
@@ -1,3 +1,16 @@
+// ------------------------------------------------------------------------------
+/*  verifyID(string,string)
+
+    Definition: This function is for profile images linked to a user's profile page.
+                This function will verify that the user is clicking a profile image
+                that is not themselves and will redirect to the follow or profile page
+                depending if the user clicks their profile image or not.
+
+    Variables:
+        id:         The id of the current user.
+        searchId:   The id of the user who the current user clicked on.
+*/
+// ------------------------------------------------------------------------------
 function verifyID(id,searchedID)
 {
   if(id == searchedID)
