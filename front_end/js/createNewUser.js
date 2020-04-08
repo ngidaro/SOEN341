08e04637b9bf117fe5cdc48dea90e2583c448525
Nicholas <ngidaro@hotmail.com>
2020-04-07 21:26:48 -0400

    Removed any unnecessary files and added comments to all functions

--- front_end/js/createNewUser.js
+++ front_end/js/createNewUser.js
@@ -1,3 +1,22 @@
+// ------------------------------------------------------------------------------
+/*  createNewUser()
+
+    Definition: On the create_account_page, the user can enter data into the textboxes.
+                This function will take the data written by the user in the textboxes and
+                using the Fetch API, will send a request to the /create_account_page.
+                This function will check to see if all the fields are populated. If the fields
+                are empty, the user will be notified. If the fields are all populated, the data will be
+                checked on the server side to see if the username is already taken. If the username is
+                not taken, the page will redirect to the user's new profile page.
+
+    Variables:
+        fname:      String of the First Name entered by the user.
+        lname:      String of the Last Name entered by the user.
+        user:       String of the username entered by the user.
+        password:   String of the password entered by the user.
+        email:      String of the email entered by the user.
+*/
+// ------------------------------------------------------------------------------
 function createNewUser()
 {
   var fname = document.getElementById("fName").value;
@@ -39,6 +58,13 @@ function createNewUser()
     })
 }
 
+// ------------------------------------------------------------------------------
+/*  emptyFields()
+
+    Definition: This function is called when the textfields on create_account_page
+                are empty or invalid. This function simply empties the fields.
+*/
+// ------------------------------------------------------------------------------
 function emptyFields()
 {
   document.getElementById("fName").value = "";
